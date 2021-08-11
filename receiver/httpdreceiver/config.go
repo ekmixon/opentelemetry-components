dc416e205a987b4458e9880f6863cbcd5612cee9
Jonathan Wamsley <jonnywamsley@gmail.com>
2021-08-11 03:16:04 -0400

    added endpoint validation

--- receiver/httpdreceiver/config.go
+++ receiver/httpdreceiver/config.go
@@ -2,6 +2,7 @@ package httpdreceiver
 
 import (
 	"fmt"
+	"net"
 	"net/url"
 	"strings"
 
@@ -14,24 +15,70 @@ type Config struct {
 	confighttp.HTTPClientSettings           `mapstructure:",squash"`
 }
 
+// TODO We need a way to validate that this is actually helping.
+//        This means:
+// 1) We have a specific scenario that fails without it.
+// 2) We have a test, that validate that the scenario fails,
+//      and that this fixes it.
+// 3) Manually validate that it fails without the modification,
+//      and that it passes with the modification.
+
+var (
+	DefaultScheme   = "http"
+	DefaultProtocol = "http://"
+	DefaultHost     = "localhost"
+	DefaultPort     = ":8081" // 8080 or 8081? we picked 8081 because nginx uses 8080
+	DefaultPath     = "/server-status"
+	ValidPath       = "/server-status/"
+	DefaultQuery    = "auto"
+)
+
 func (cfg *Config) Validate() error {
 	if cfg.Endpoint != "" {
+		cfg.Endpoint = strings.ToLower(cfg.Endpoint)
 
-		// TODO We need a way to validate that this is actually helping.
-		//        This means:
-		// 1) We have a specific scenario that fails without it.
-		// 2) We have a test, that validate that the scenario fails,
-		//      and that this fixes it.
-		// 3) Manually validate that it fails without the modification,
-		//      and that it passes with the modification.
-		if !strings.HasPrefix(strings.ToLower(cfg.Endpoint), "http") {
-			cfg.Endpoint = fmt.Sprintf("http://%s", cfg.Endpoint)
+		if !strings.HasPrefix(cfg.Endpoint, DefaultScheme) { // adds default schema
+			cfg.Endpoint = fmt.Sprintf("%s%s", DefaultProtocol, cfg.Endpoint)
 		}
 
-		_, err := url.Parse(cfg.Endpoint)
+		u, err := url.Parse(cfg.Endpoint) // err will never fail here
 		if err != nil {
-			return fmt.Errorf("invalid url specified in field 'endpoint'")
+			return fmt.Errorf("invalid endpoint '%s'", cfg.Endpoint)
 		}
+
+		if u.Port() == "" { // adds default port
+			u.Host = u.Host + DefaultPort
+		}
+
+		if !(net.ParseIP(u.Hostname()) != nil || u.Hostname() == DefaultHost) { // validates either IP address or local
+			return fmt.Errorf("invalid host: expected IP address or localhost, but got %s", u.Hostname())
+		}
+
+		if u.Path == "" { // adds default path
+			u.Path = DefaultPath
+		} else if !(u.Path == DefaultPath || u.Path == ValidPath) {
+			return fmt.Errorf("invalid path: expected '%s' path, but got '%s'", DefaultPath, u.Path)
+		}
+
+		if u.RawQuery == "" { // adds default query
+			u.RawQuery = DefaultQuery
+		} else if u.RawQuery != DefaultQuery {
+			return fmt.Errorf("invalid raw query: expected '%s' query, but got '%s'", DefaultQuery, u.RawQuery)
+		}
+
+		if u.Fragment != "" { // validates no fragments
+			return fmt.Errorf("invalid fragment: expected empty string, but got %s", u.Fragment)
+		}
+
+		cfg.Endpoint = u.String()
+	} else {
+		u := url.URL{
+			Scheme:   DefaultScheme,
+			Host:     DefaultHost + DefaultPort,
+			Path:     DefaultPath,
+			RawQuery: DefaultQuery,
+		}
+		cfg.Endpoint = u.String()
 	}
 	return nil
 }
--- receiver/httpdreceiver/config_test.go
+++ receiver/httpdreceiver/config_test.go
@@ -1,47 +1,149 @@
 package httpdreceiver
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/collector/config/confighttp"
 )
 
-func TestDefaultConfig(t *testing.T) {
-
-}
-
 func TestValidate(t *testing.T) {
+	t.Run("error path", func(t *testing.T) {
+		testCases := []struct {
+			desc   string
+			rawUrl string
+			actual error
+		}{
+			{
+				desc:   "only port",
+				rawUrl: "8081",
+				actual: errors.New("invalid host: expected IP address or localhost, but got 8081"),
+			},
+			{
+				desc:   "invalid host",
+				rawUrl: "127.0.0.0.1.1",
+				actual: errors.New("invalid host: expected IP address or localhost, but got 127.0.0.0.1.1"),
+			},
+			{
+				desc:   "invalid host, port",
+				rawUrl: "127.0.0.0.1.1:8080",
+				actual: errors.New("invalid host: expected IP address or localhost, but got 127.0.0.0.1.1"),
+			},
+			{
+				desc:   "invalid path",
+				rawUrl: "127.0.0.1:8080/bad-server-status",
+				actual: errors.New("invalid path: expected '/server-status' path, but got '/bad-server-status'"),
+			},
+			{
+				desc:   "invalid path, path",
+				rawUrl: "127.0.0.1:8080/server-status/server-status",
+				actual: errors.New("invalid path: expected '/server-status' path, but got '/server-status/server-status'"),
+			},
+			{
+				desc:   "invalid query",
+				rawUrl: "127.0.0.1:8080/server-status?badauto",
+				actual: errors.New("invalid raw query: expected 'auto' query, but got 'badauto'"),
+			},
+			{
+				desc:   "invalid frag",
+				rawUrl: "127.0.0.1:8080/server-status?auto#badfrag",
+				actual: errors.New("invalid fragment: expected empty string, but got badfrag"),
+			},
+			{
+				desc:   "missing colon",
+				rawUrl: "127.0.0.18080/server-status?auto",
+				actual: errors.New("invalid host: expected IP address or localhost, but got 127.0.0.18080"),
+			},
+			{
+				desc:   "missing colon with space",
+				rawUrl: "127.0.0.1 8080/server-status?auto",
+				actual: errors.New("invalid endpoint 'http://127.0.0.1 8080/server-status?auto'"),
+			},
+		}
+		for _, tC := range testCases {
+			t.Run(tC.desc, func(t *testing.T) {
+				cfg := NewFactory().CreateDefaultConfig().(*Config)
+				cfg.Endpoint = tC.rawUrl
+				require.EqualError(t, cfg.Validate(), tC.actual.Error())
+			})
+		}
+	})
 
-	needsFixup := []string{
-		"127.0.0.1/server-status?auto",
-		"localhost:8081/server-status?auto",
-	}
-	testCases := []string{
-		"http://127.0.0.1/server-status?auto",
-		"https://127.0.0.1/server-status?auto",
-		"HTTP://127.0.0.1/server-status?auto",
-		"HTTPS://127.0.0.1/server-status?auto",
-		"http://localhost:8081/server-status?auto",
-		"https://localhost:8081/server-status?auto",
-		"HTTP://localhost:8081/server-status?auto",
-		"HTTPS://localhost:8081/server-status?auto",
-	}
-
-	for _, tc := range needsFixup {
-		t.Run(tc, func(t *testing.T) {
-			cfg := NewFactory().CreateDefaultConfig().(*Config)
-			require.Error(t, cfg.Validate())
-			cfg.Fixup()
-			require.NoError(t, cfg.Validate())
-		})
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc, func(t *testing.T) {
-			cfg := NewFactory().CreateDefaultConfig().(*Config)
-			cfg.Endpoint = tc
-			require.NoError(t, cfg.Validate())
-		})
-	}
+	t.Run("happy path", func(t *testing.T) {
+		testCases := []struct {
+			desc      string
+			rawUrl    string
+			actual    error
+			parsedUrl string
+		}{
+			{
+				desc:      "default path",
+				rawUrl:    "",
+				actual:    nil,
+				parsedUrl: "http://localhost:8081/server-status?auto",
+			},
+			{
+				desc:      "only host(local)",
+				rawUrl:    "localhost",
+				actual:    nil,
+				parsedUrl: "http://localhost:8081/server-status?auto",
+			},
+			{
+				desc:      "only host",
+				rawUrl:    "127.0.0.1",
+				actual:    nil,
+				parsedUrl: "http://127.0.0.1:8081/server-status?auto",
+			},
+			{
+				desc:      "only host(local), port",
+				rawUrl:    "localhost:8081",
+				actual:    nil,
+				parsedUrl: "http://localhost:8081/server-status?auto",
+			},
+			{
+				desc:      "only host, port",
+				rawUrl:    "127.0.0.1:8081",
+				actual:    nil,
+				parsedUrl: "http://127.0.0.1:8081/server-status?auto",
+			},
+			{
+				desc:      "only host, port, path",
+				rawUrl:    "127.0.0.1:8081/server-status",
+				actual:    nil,
+				parsedUrl: "http://127.0.0.1:8081/server-status?auto",
+			},
+			{
+				desc:      "only host, port, path, query",
+				rawUrl:    "127.0.0.1:8081/server-status?auto",
+				actual:    nil,
+				parsedUrl: "http://127.0.0.1:8081/server-status?auto",
+			},
+			{
+				desc:      "only schema, host, port, path, query",
+				rawUrl:    "http://127.0.0.1:8081/server-status?auto",
+				actual:    nil,
+				parsedUrl: "http://127.0.0.1:8081/server-status?auto",
+			},
+			{
+				desc:      "only schema(https), host, port, path, query",
+				rawUrl:    "https://127.0.0.1:8081/server-status?auto",
+				actual:    nil,
+				parsedUrl: "https://127.0.0.1:8081/server-status?auto",
+			},
+			{
+				desc:      "added slash",
+				rawUrl:    "127.0.0.1:8081/server-status/?auto",
+				actual:    errors.New("invalid path: expected '/server-status' path, but got '/server-status/'"),
+				parsedUrl: "http://127.0.0.1:8081/server-status/?auto",
+			},
+		}
+		for _, tC := range testCases {
+			t.Run(tC.desc, func(t *testing.T) {
+				cfg := NewFactory().CreateDefaultConfig().(*Config)
+				cfg.Endpoint = tC.rawUrl
+				require.NoError(t, cfg.Validate())
+				require.Equal(t, cfg.Endpoint, tC.parsedUrl)
+			})
+		}
+	})
 }
