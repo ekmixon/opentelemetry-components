6abebedbebffaf70bea0d32088522e5d6123e04e
Jonathan Wamsley <jonathan.wamsley@bluemedora.com>
2021-09-10 15:08:10 -0400

    added integration test for version 6.6

--- receiver/couchbasereceiver/integration_test.go
+++ receiver/couchbasereceiver/integration_test.go
@@ -19,40 +19,40 @@ import (
 )
 
 func TestCouchbaseIntegration(t *testing.T) {
-	// t.Run("Running docker version 6.6 on port 8092", func(t *testing.T) {
-	// 	container := getContainer(t, containerRequest6_6)
-	// 	defer func() {
-	// 		require.NoError(t, container.Terminate(context.Background()))
-	// 	}()
-	// 	hostname, err := container.Host(context.Background())
-	// 	require.NoError(t, err)
+	t.Run("Running docker version 6.6", func(t *testing.T) {
+		container := getContainer(t, containerRequest6_6)
+		defer func() {
+			require.NoError(t, container.Terminate(context.Background()))
+		}()
+		hostname, err := container.Host(context.Background())
+		require.NoError(t, err)
 
-	// 	f := NewFactory()
-	// 	cfg := f.CreateDefaultConfig().(*Config)
-	// 	cfg.Endpoint = net.JoinHostPort(hostname, "8092")
-	// 	cfg.Username = "otelu"
-	// 	cfg.Password = "otelpassword"
+		f := NewFactory()
+		cfg := f.CreateDefaultConfig().(*Config)
+		cfg.Endpoint = fmt.Sprintf("http://%s", net.JoinHostPort(hostname, "8091"))
+		cfg.Username = "otelu"
+		cfg.Password = "otelpassword"
 
-	// 	consumer := new(consumertest.MetricsSink)
-	// 	settings := componenttest.NewNopReceiverCreateSettings()
-	// 	rcvr, err := f.CreateMetricsReceiver(context.Background(), settings, cfg, consumer)
-	// 	require.NoError(t, err, "failed creating metrics receiver")
-	// 	require.NoError(t, rcvr.Start(context.Background(), componenttest.NewNopHost()))
-	// 	require.Eventuallyf(t, func() bool {
-	// 		return len(consumer.AllMetrics()) > 0
-	// 	}, 2*time.Minute, 1*time.Second, "failed to receive more than 0 metrics")
+		consumer := new(consumertest.MetricsSink)
+		settings := componenttest.NewNopReceiverCreateSettings()
+		rcvr, err := f.CreateMetricsReceiver(context.Background(), settings, cfg, consumer)
+		require.NoError(t, err, "failed creating metrics receiver")
+		require.NoError(t, rcvr.Start(context.Background(), componenttest.NewNopHost()))
+		require.Eventuallyf(t, func() bool {
+			return len(consumer.AllMetrics()) > 0
+		}, 2*time.Minute, 1*time.Second, "failed to receive more than 0 metrics")
 
-	// 	md := consumer.AllMetrics()[0]
-	// 	require.Equal(t, 1, md.ResourceMetrics().Len())
-	// 	ilms := md.ResourceMetrics().At(0).InstrumentationLibraryMetrics()
-	// 	require.Equal(t, 1, ilms.Len())
-	// 	metrics := ilms.At(0).Metrics()
-	// 	require.NoError(t, rcvr.Shutdown(context.Background()))
+		md := consumer.AllMetrics()[0]
+		require.Equal(t, 1, md.ResourceMetrics().Len())
+		ilms := md.ResourceMetrics().At(0).InstrumentationLibraryMetrics()
+		require.Equal(t, 1, ilms.Len())
+		metrics := ilms.At(0).Metrics()
+		require.NoError(t, rcvr.Shutdown(context.Background()))
 
-	// 	validateIntegrationResult(t, metrics)
-	// })
+		validateIntegrationResult(t, metrics)
+	})
 
-	t.Run("Running docker version 7.0 on port 8091", func(t *testing.T) {
+	t.Run("Running docker version 7.0", func(t *testing.T) {
 		container := getContainer(t, containerRequest7_0)
 		defer func() {
 			require.NoError(t, container.Terminate(context.Background()))
@@ -87,15 +87,15 @@ func TestCouchbaseIntegration(t *testing.T) {
 }
 
 var (
-	// containerRequest6_6 = testcontainers.ContainerRequest{
-	// 	FromDockerfile: testcontainers.FromDockerfile{
-	// 		Context:    path.Join(".", "testdata"),
-	// 		Dockerfile: "Dockerfile.couchbase.6_6",
-	// 	},
-	// 	ExposedPorts: []string{"8091:8091"},
-	// 	WaitingFor: wait.ForListeningPort("8091").
-	// 		WithStartupTimeout(2 * time.Minute),
-	// }
+	containerRequest6_6 = testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context:    path.Join(".", "testdata"),
+			Dockerfile: "Dockerfile.couchbase.6_6",
+		},
+		ExposedPorts: []string{"8091:8091"},
+		WaitingFor: wait.ForListeningPort("8091").
+			WithStartupTimeout(5 * time.Minute),
+	}
 	containerRequest7_0 = testcontainers.ContainerRequest{
 		FromDockerfile: testcontainers.FromDockerfile{
 			Context:    path.Join(".", "testdata"),
@@ -107,14 +107,6 @@ var (
 	}
 )
 
-type TestLogConsumer struct {
-	t *testing.T
-}
-
-func (lc *TestLogConsumer) Accept(l testcontainers.Log) {
-	lc.t.Log(string(l.Content))
-}
-
 func getContainer(t *testing.T, req testcontainers.ContainerRequest) testcontainers.Container {
 	require.NoError(t, req.Validate())
 	container, err := testcontainers.GenericContainer(
@@ -124,10 +116,6 @@ func getContainer(t *testing.T, req testcontainers.ContainerRequest) testcontain
 			Started:          true,
 		})
 	require.NoError(t, err)
-
-	logConsumer := TestLogConsumer{t}
-	require.NoError(t, container.StartLogProducer(context.Background()))
-	container.FollowOutput(&logConsumer)
 
 	code, err := container.Exec(context.Background(), []string{"/setup.sh"})
 	require.NoError(t, err)
