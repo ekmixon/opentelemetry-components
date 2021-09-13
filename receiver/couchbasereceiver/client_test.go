342d542bb6c00b570d31f37ccffbb0ab2bdd1cd8
Jonathan Wamsley <jonathan.wamsley@bluemedora.com>
2021-09-13 07:56:14 -0400

    added client test

--- receiver/couchbasereceiver/client_test.go
+++ receiver/couchbasereceiver/client_test.go
@@ -26,7 +26,7 @@ func TestNewCouchbaseClient(t *testing.T) {
 			}}, zap.NewNop())
 
 		require.Nil(t, couchbaseClient)
-		require.NotNil(t, err)
+		require.Error(t, err)
 	})
 	t.Run("no error", func(t *testing.T) {
 		couchbaseMock := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
@@ -43,86 +43,242 @@ func TestNewCouchbaseClient(t *testing.T) {
 				Endpoint: couchbaseMock.URL + "/pools/default",
 			},
 		}, zap.NewNop())
-		require.Nil(t, err)
+		require.NoError(t, err)
 		require.NotNil(t, couchbaseClient)
 	})
 }
 
-func TestGetNodeSTats(t *testing.T) {
+func TestGetNodeStats(t *testing.T) {
 	couchbaseMock := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		rw.WriteHeader(200)
-		_, _ = rw.Write([]byte(`{"key": "value"}`))
-		// if req.URL.Path == "/pools/default" {
-		// 	rw.WriteHeader(200)
-		// 	_, _ = rw.Write([]byte(`{"key": "value"}`))
-		// 	return
-		// }
-		// if req.URL.Path == "/invalid_endpoint" {
-		// 	rw.WriteHeader(404)
-		// 	return
-		// }
-		// if req.URL.Path == "/invalid_body" {
-		// 	rw.Header().Set("Content-Length", "1")
-		// 	return
-		// }
-		// if req.URL.Path == "/invalid_json" {
-		// 	rw.WriteHeader(200)
-		// 	_, _ = rw.Write([]byte(`{"}`))
-		// 	return
-		// }
-
-		// rw.WriteHeader(404)
+		if req.URL.Path == "/pools/default/invalid_endpoint" {
+			rw.WriteHeader(404)
+			return
+		}
+		if req.URL.Path == "/invalid_read/pools/default" {
+			rw.Header().Set("Content-Length", "1")
+			return
+		}
+		if req.URL.Path == "/invalid_json/pools/default" {
+			rw.WriteHeader(200)
+			return
+		}
+		if req.URL.Path == "/no_error/pools/default" {
+			rw.WriteHeader(200)
+			_, _ = rw.Write([]byte(`{}`))
+			return
+		}
 	}))
-	// t.Run("invalid endpoint", func(t *testing.T) {
-	// 	couchdbClient, err := newCouchDBClient(componenttest.NewNopHost(), &Config{
-	// 		HTTPClientSettings: confighttp.HTTPClientSettings{
-	// 			Endpoint: couchbaseMock.URL + "/invalid_endpoint",
-	// 		},
-	// 	}, zap.NewNop())
-	// 	require.Nil(t, err)
-	// 	require.NotNil(t, couchdbClient)
-
-	// 	result, err := couchdbClient.Get()
-	// 	require.NotNil(t, err)
-	// 	require.Nil(t, result)
-	// })
-	// t.Run("invalid body", func(t *testing.T) {
-	// 	couchdbClient, err := newCouchDBClient(componenttest.NewNopHost(), &Config{
-	// 		HTTPClientSettings: confighttp.HTTPClientSettings{
-	// 			Endpoint: couchbaseMock.URL + "/invalid_body",
-	// 		},
-	// 	}, zap.NewNop())
-	// 	require.Nil(t, err)
-	// 	require.NotNil(t, couchdbClient)
-
-	// 	result, err := couchdbClient.Get()
-	// 	require.NotNil(t, err)
-	// 	require.Nil(t, result)
-	// })
-	// t.Run("invalid json", func(t *testing.T) {
-	// 	couchdbClient, err := newCouchDBClient(componenttest.NewNopHost(), &Config{
-	// 		HTTPClientSettings: confighttp.HTTPClientSettings{
-	// 			Endpoint: couchbaseMock.URL + "/invalid_json",
-	// 		},
-	// 	}, zap.NewNop())
-	// 	require.Nil(t, err)
-	// 	require.NotNil(t, couchdbClient)
-
-	// 	result, err := couchdbClient.Get()
-	// 	require.NotNil(t, err)
-	// 	require.Nil(t, result)
-	// })
-	t.Run("no error", func(t *testing.T) {
-		couchdbClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+	t.Run("error on NewRequest", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
 			HTTPClientSettings: confighttp.HTTPClientSettings{
-				Endpoint: couchbaseMock.URL + "/pools/default",
+				Endpoint: couchbaseMock.URL + "request with space",
 			},
 		}, zap.NewNop())
-		require.Nil(t, err)
-		require.NotNil(t, couchdbClient)
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
 
-		result, err := couchdbClient.getNodeStats()
-		require.Nil(t, err)
+		result, err := couchbaseClient.getNodeStats()
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+
+	t.Run("error on Do", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: "",
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.getNodeStats()
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+	t.Run("error on read", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseMock.URL + "/invalid_read",
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.getNodeStats()
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+	t.Run("error on unmarshal", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseMock.URL + "/invalid_json",
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.getNodeStats()
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+	t.Run("no error", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseMock.URL + "/no_error",
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.getNodeStats()
+		require.NoError(t, err)
+		require.NotNil(t, result)
+	})
+}
+
+func TestGetBucketsStats(t *testing.T) {
+	couchbaseMock := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/invalid_read" {
+			rw.Header().Set("Content-Length", "1")
+			return
+		}
+		if req.URL.Path == "/invalid_json" {
+			rw.WriteHeader(200)
+			return
+		}
+		if req.URL.Path == "/pools/default/buckets" {
+			rw.WriteHeader(200)
+			_, _ = rw.Write([]byte(`[{}]`))
+			return
+		}
+	}))
+	t.Run("error on NewRequest", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseMock.URL,
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.getBucketsStats("request with space")
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+	t.Run("error on Do", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: "",
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.getBucketsStats("/invalid_request")
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+	t.Run("error on read", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseMock.URL,
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.getBucketsStats("/invalid_read")
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+	t.Run("error on unmarshal", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseMock.URL,
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.getBucketsStats("/invalid_json")
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+	t.Run("no error", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseMock.URL,
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.getBucketsStats("/pools/default/buckets")
+		require.NoError(t, err)
+		require.NotNil(t, result)
+	})
+}
+
+func TestGet(t *testing.T) {
+	couchbaseNodeStatsMock := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/invalid_json" {
+			rw.WriteHeader(200)
+			return
+		}
+		if req.URL.Path == "/error_on_buckets/pools/default" {
+			rw.WriteHeader(200)
+			_, _ = rw.Write([]byte(`{}`))
+			return
+		}
+		if req.URL.Path == "/no_error/pools/default" {
+			rw.WriteHeader(200)
+			_, _ = rw.Write([]byte(`{
+				"buckets": {"uri": "/pools/default/buckets?v=105443202&uuid=4517d7f94cde4b64cdd260a662c65072"}}`))
+			return
+		}
+		if req.URL.Path == "/no_error/pools/default/buckets" {
+			rw.WriteHeader(200)
+			_, _ = rw.Write([]byte(`[{}]`))
+			return
+		}
+	}))
+	t.Run("error on nodestats", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseNodeStatsMock.URL + "/invalid_json",
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.Get()
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+	t.Run("error on bucketsStats", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseNodeStatsMock.URL + "/error_on_buckets",
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.Get()
+		require.Error(t, err)
+		require.Nil(t, result)
+	})
+	t.Run("no error", func(t *testing.T) {
+		couchbaseClient, err := newCouchbaseClient(componenttest.NewNopHost(), &Config{
+			HTTPClientSettings: confighttp.HTTPClientSettings{
+				Endpoint: couchbaseNodeStatsMock.URL + "/no_error",
+			},
+		}, zap.NewNop())
+		require.NoError(t, err)
+		require.NotNil(t, couchbaseClient)
+
+		result, err := couchbaseClient.Get()
+		require.NoError(t, err)
 		require.NotNil(t, result)
 	})
 }
@@ -132,6 +288,6 @@ func TestBasicAuth(t *testing.T) {
 	password := "otelp"
 	encoded := basicAuth(username, password)
 	decoded, err := base64.StdEncoding.DecodeString(encoded)
-	require.Nil(t, err)
+	require.NoError(t, err)
 	require.Equal(t, "otelu:otelp", string(decoded))
 }
