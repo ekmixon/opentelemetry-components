3fcf0cd77f4706d43863887ec78c789251bfba28
Mrod1598 <miguel.rodriguez@observiq.com>
2021-10-01 10:54:09 -0400

    Swap to mdatagen in contrib (#82)

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -9,5 +9,6 @@ package tools
 import (
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/open-telemetry/opentelemetry-collector-contrib/cmd/mdatagen"
 	_ "golang.org/x/tools/cmd/goimports"
 )
